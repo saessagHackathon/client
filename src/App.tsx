5fb85950d5b1fa4996b060c5914a73f61e7d4f16
Kim Min-gyu <99083803+cobocho@users.noreply.github.com>
2024-07-06 18:52:38 +0900

    feat: tts 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import { useState } from 'react'
+import { useLayoutEffect, useState } from 'react'
+
+import { getSpeech } from '@utils/tts'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [line, setLine] = useState('')
+
+  useLayoutEffect(() => {
+    window.speechSynthesis.getVoices()
+  }, [])
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLine(e.target.value)
+  }
+
+  const handleSpeech = () => {
+    getSpeech(line)
+  }
 
   return (
     <>
       <h1 className="text-wrap text-8xl">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <input type="text" onChange={handleInputChange} />
+      <button onClick={handleSpeech}>말하기</button>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
---
+++ src/utils/tts.ts
@@ -0,0 +1,34 @@
+export const getSpeech = (text: string) => {
+  let voices: SpeechSynthesisVoice[] = []
+
+  const setVoiceList = () => {
+    voices = window.speechSynthesis.getVoices()
+  }
+
+  setVoiceList()
+
+  if (window.speechSynthesis.onvoiceschanged !== undefined) {
+    window.speechSynthesis.onvoiceschanged = setVoiceList
+  }
+
+  const speech = (line: string) => {
+    const lang = 'ko-KR'
+    const utterThis = new SpeechSynthesisUtterance(line)
+
+    utterThis.lang = lang
+
+    const korVoice = voices.find((elem) => elem.voiceURI === 'Google 한국의')
+
+    console.log(korVoice)
+
+    if (korVoice) {
+      utterThis.voice = korVoice
+    } else {
+      return
+    }
+
+    window.speechSynthesis.speak(utterThis)
+  }
+
+  speech(text)
+}
