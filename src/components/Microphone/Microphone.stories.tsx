16aa21b9b05e6d3096badbfbba08d416ff33d0d1
Kim Min-gyu <rlaalsrb1111@naver.com>
2024-07-14 21:01:39 +0900

    feat: 마이크 컴포넌트 구현

---
+++ src/components/Microphone/Microphone.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Microphone } from './Microphone'
+
+const meta = {
+  title: 'Microphone',
+  component: Microphone,
+  args: {
+    size: 'lg',
+  },
+  argTypes: {
+    size: {
+      options: ['lg', 'md'],
+      control: { type: 'select' },
+      description: '마이크 크기',
+    },
+  },
+  parameters: ['autodocs'],
+} satisfies Meta<typeof Microphone>
+
+export default meta
+
+type Story = StoryObj<typeof Microphone>
+
+export const Playground: Story = {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const px0to100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) }
+const px0to300 = { ...Array.from(Array(301)).map((_, i) => `${i}px`) }
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -8,17 +8,21 @@ export default {
       colors: {
         primary: '#FF5757',
       },
-      width: px0to100,
-      height: px0to100,
-      fontSize: px0to100,
-      padding: px0to100,
+      width: px0to300,
+      height: px0to300,
+      fontSize: px0to300,
+      padding: px0to300,
       borderRadius: {
         32: '32px',
         12: '12px',
         8: '8px',
         4: '4px',
       },
+      boxShadow: {
+        sm: '0px 0px 4px rgba(0, 0, 0, 0.25)',
+      },
     },
   },
   plugins: [],
 }
+/* Ellipse 6716 */
